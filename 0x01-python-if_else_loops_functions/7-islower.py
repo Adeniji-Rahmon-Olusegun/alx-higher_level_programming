452334607e98a4a169ee8644a980a00130e9a7ae
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-03 13:13:28 -0800

    7-islower.py file is added

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,6 +3,6 @@
 def islower(c):
     """ Checks if a character is lower case."""
 
-    if (ord(c) is >= 97 and ord(c) <= 122):
+    if (ord(c) >= 97 and ord(c) <= 122):
         return (True)
     return (False)
