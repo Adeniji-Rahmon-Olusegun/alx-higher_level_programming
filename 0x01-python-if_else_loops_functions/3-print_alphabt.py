d9a38dc381ad4742458c50b8b018d2702ddfdae3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-03 09:48:40 -0800

    3-print_alphabt.py file added

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for lcqe in range(ord('a') , (ord('z') + 1)):
+for lcqe in range(ord('a'), (ord('z') + 1)):
     if ((chr(lcqe) != 'e') and (chr(lcqe) != 'q')):
         print("{0}".format(chr(lcqe)), end="")
