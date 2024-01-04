65d017d0059b8f5cdca1752296ebba02b4f793e2
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-04 10:29:49 -0800

    0-add.py file added

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Addition function
+
+    Args:
+        a: integer1
+        b: integer2
+
+    Returns:
+            Return value: a + b
+    """
+
+    return (a + b)
