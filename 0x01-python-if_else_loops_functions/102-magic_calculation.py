19f37df2a550387d59e4e1990a64d954710c459f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-03 17:52:11 -0800

    102-magic_calculation.py file added

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """It presents a code that is same as its byte code"""
+
+    if (a < b):
+        return (c)
+    elif (c > b):
+        return (a + b)
+    return ((a * b) - c)
