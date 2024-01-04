239d76d4d436637593b59beecd4b857178194b3d
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-03 16:45:17 -0800

    100-print_tebahpla.py file added

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for rvalp in range(ord('z'), (ord('a') - 1), -1):
-    print("{}".format(chr(rvalp)), end="")
+for rv in range(ord('z'), (ord('a') - 1), -1):
+    print("{}".format(chr(rv - 32) if (rv % 2 != 0) else chr(rv)), end="")
