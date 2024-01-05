129073fb187ee03f7ac965abcbbf8356e6e145f0
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-04 16:30:28 -0800

    3-infinite_add.py file added

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,10 +7,8 @@ if __name__ == "__main__":
     sums = 0
 
     if (len(sys.argv) == 1):
-        print("{}".format(0))
-    elif(len(sys.argv) == 2):
-        print("{}".format(int(sys.argv[1]) + 0))
-    elif (len(sys.argv) > 2):
+        print("{}".format(sums))
+    else:
         for arg in range(1, len(sys.argv)):
             sums += int(sys.argv[arg])
         print("{}".format(sums))
