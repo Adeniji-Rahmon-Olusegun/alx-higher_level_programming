637dbd81e2d91d4999eb7ff1f7445374fca11b39
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-04 16:52:18 -0800

    4-hidden_discovery.py file added

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from hidden_4 import *
+
+    def_names_pyc = dir()
+
+    for names in range(len(def_names_pyc)):
+        if (names[:2] != "__"):
+            print("{:s}".format(def_names_pyc[names]))
