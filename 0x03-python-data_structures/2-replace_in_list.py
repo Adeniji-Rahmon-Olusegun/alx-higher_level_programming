e76015757e5353615dfca08349ba160248f4139a
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-06 01:56:10 -0800

    2-replace_in_list.py file added

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replaces en element of a list with another
+
+    Args:
+        my_list: list of elemets
+        idx: index position of element to be replaced
+        element: for replacement
+
+    Return:
+            Update list
+            my_list: if idx is -ve
+            my_list: if idx >= len(my_list)
+    """
+    if (idx < 0):
+        return (my_list)
+    elif (idx >= len(my_list)):
+        return (my_list)
+    else:
+        update_list = my_list[idx] = element
+        return (update_list)
