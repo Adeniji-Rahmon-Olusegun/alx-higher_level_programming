c5b402911e15a212c9e1e65ae1fddf2e64dc1b78
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-06 01:18:47 -0800

    0-print_list_integer.py file added

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        """
-            Prints all integers in a list
+def print_list_integer(my_list=[]):
+    """
+        Prints all integers in a list
 
-            Args:
-                my_list: list of integers
+        Args:
+            my_list: list of integers
 
-            Return:
-                    None
-        """
-        for obj in my_list:
-            print("{}".format(obj))
+        Return:
+                None
+    """
+    for obj in my_list:
+        print("{}".format(obj))
