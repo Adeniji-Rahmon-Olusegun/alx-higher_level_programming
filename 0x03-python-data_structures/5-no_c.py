826eca6df3b42f829e789d6ebd498a0f798d5c63
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-06 03:37:23 -0800

    5-no_c.py added

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """
+    Removes all character c and C from a string
+
+    Args:
+        my_string
+
+    Return:
+            new_string
+    """
+    if my_string:
+
+        new_string = ""
+
+        for char_check in my_string:
+            if char_check != 'c' and char_check != 'C':
+                new_string += char_check
+
+        return (new_string)
