2eceac4c95a25a0b3e9bbef577fcab647b41a769
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-06 02:22:26 -0800

    3-print_reversed_list_integer.py file added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -10,5 +10,6 @@ def print_reversed_list_integer(my_list=[]):
     Return:
             None
     """
-    for obj in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[obj]))
+    if (my_list):
+        for obj in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[obj]))
