746c8a5427ad2108002aeb432091374606f75c80
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-06 17:23:08 -0800

    11-delete_at.py file added

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    """
+    Deletes item at a specific index position
+
+    Args:
+        list: my_list
+        int: idx (index postion)
+
+    Return:
+            list: modified list
+    """
+    if my_list:
+
+        if idx >= len(my_list):
+            return my_list
+
+        del my_list[idx]
+
+        return my_list
