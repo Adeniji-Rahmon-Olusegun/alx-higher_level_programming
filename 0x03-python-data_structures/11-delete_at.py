c6686167771c5b403768c6decaed9626a1cd5310
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-07 05:09:21 -0800

    11-delete_at.py file added

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -11,11 +11,10 @@ def delete_at(my_list=[], idx=0):
     Return:
             list: modified list
     """
-    if my_list:
 
-        if idx < 0 or idx >= len(my_list):
-            return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
 
-        new_list_after_deletion = my_list[:idx] + my_list[(idx + 1):]
+    del my_list[idx]
 
-        return new_list_after_deletion
+    return my_list
