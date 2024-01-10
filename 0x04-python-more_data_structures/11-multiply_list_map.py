4e0183132b736366eb15110eb810dad1d781ab2d
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-10 09:31:59 -0800

    11-multiply_list_map.py file added

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    Returns a list with all values multiplied by a number
-
-    Args:
-        my_list: list
-        number: integer
-
-    Return:
-          list
-    """
-    mod_list = [ml for ml in my_list]
-    return list(map(lambda arg: arg * number, mod_list))
+    return list(map(lambda arg: arg * number, [ml for ml in my_list]))
