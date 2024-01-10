bba5a57d8b4a4adfe008c39493a58ac781bf3411
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-10 06:43:17 -0800

    4-only_diff_elements.py file added

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """
+    Returns a set of all elements present in only one set
+
+    Args:
+        set_1: set
+        Set_2: set
+
+    Return:
+          set
+    """
+    return set1_1.union(set_2) - set1_1.intersection(set_2)
