0324bb4e6e054e5300a837cae1bfd273b597a586
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-10 05:56:42 -0800

    0-square_matrix_simple.py file added

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -11,12 +11,12 @@ def square_matrix_simple(matrix=[]):
             Matrix containing the square of values in the
             original matrix
     """
-    squared_matrix = matrix.copy()
+    squared_matrix = [[0 for i in L1] for L1 in matrix]
 
     dic_m = {"dmatrix": squared_matrix}
 
     for matl in range(len(dic_m["dmatrix"])):
         for idx in range(len(dic_m["dmatrix"][matl])):
-            dic_m["dmatrix"][matl][idx] = (dic_m["dmatrix"][matl][idx])**2
+            dic_m["dmatrix"][matl][idx] = (matrix[matl][idx])**2
 
     return (dic_m["dmatrix"])
