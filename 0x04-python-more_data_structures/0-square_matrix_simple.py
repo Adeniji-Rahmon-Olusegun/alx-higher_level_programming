9bcf97e871130283404b975c5b21cbdf5a7a467f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-10 05:28:38 -0800

    0-square_matrix_simple.py file added

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """
+    Computes the square of all integers of a matrix
+
+    Args:
+        matrix: List of list
+
+    Return:
+            Matrix containing the square of values in the
+            original matrix
+    """
+    squared_matrix = matrix.copy()
+
+    dic_m = {"dmatrix": squared_matrix}
+
+    for matl in range(len(dic_m["dmatrix"])):
+        for idx in range(len(dic_m["dmatrix"][mat]l)):
+            dic_m["dmatrix"][matl][idx] = (dic_m["dmatrix"][matl][idx])**2
+
+    return (dic_m["dmatrix"])
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,9 @@
+Data Structures like Sets and Dictionaries In Python have been illustrated in this directory by solving some exercises.
+
+- The exercises have been completely prepared by ALX
+
+- The python codes/srcipts are pycodestyle compliant
+
+- All files are  interpreted/compiled on Ubuntu 20.04 LTS using python3 (version 3.8.5)
+
+- All files are executable
