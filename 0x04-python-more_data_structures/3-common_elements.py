82561e9ae0d8690788b4fdcb88a13672465a2b8f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-10 06:31:31 -0800

    3-common_elements.py file added

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    Returs a set of common elements in two sets
+
+    Args:
+        set_1: set
+        set_2: set
+
+    Return:
+            set
+    """
+
+    return set_1.intersection(set_2)
