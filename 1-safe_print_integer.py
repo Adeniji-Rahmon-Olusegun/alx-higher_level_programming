4d3652adc91837afd7533ac1e5198c9b65a6835f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-22 14:24:33 -0800

    1-safe_print_integer.py file added

---
+++ 1-safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+    Prints an integer
+
+    Args:
+        value: (str, int, etc.,)
+
+    Return:
+          Bool
+    """
+
+    try:
+        real_val = int(value)
+        print("{:d}".format(real_val))
+        return True
+    except ValueError:
+        return False
