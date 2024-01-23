a27c90ecaef9058731ac57dd3f8045b5d345364e
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-22 17:46:54 -0800

    100-safe_print_integer_err.py file added

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -15,6 +15,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as ve:
-        sys.stderr.write("Exception: {}\n".format(ve))
+    except Exception as vte:
+        sys.stderr.write("Exception: {}\n".format(vte))
         return False
