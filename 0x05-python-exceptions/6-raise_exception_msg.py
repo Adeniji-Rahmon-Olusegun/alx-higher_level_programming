0ce43afce36f6c87517d7c2ca3a84b524a2c208c
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-22 17:15:55 -0800

    6-raise_exception_msg.py file added

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a message
+
+    Args:
+        message: str
+
+    Return:
+            None
+    """
+    raise NameError(message)
