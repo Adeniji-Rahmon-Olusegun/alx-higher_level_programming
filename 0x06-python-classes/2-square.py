6f09ed503f1396a0f76b16e378f58938a694538b
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-24 08:27:57 -0800

    2-square.py file added

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,14 +12,9 @@ class Square:
         size (int): length/breadth of a square
     """
     def __init__(self, size=0):
-        try:
-            if not isinstance(size, int):
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
-        else:
-            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
