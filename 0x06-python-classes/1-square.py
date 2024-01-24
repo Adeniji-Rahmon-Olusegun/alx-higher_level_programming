31dfc1e58f4e9068891ecbf7f8daf63cfc357fd3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-24 05:54:46 -0800

    1-square.py file added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class with square attribute"""
+
+
+class Square:
+    """Has the size of sqaure as private instance attribute
+
+    __init__ clares the size attribute as private instance attribute
+
+    Atributes:
+        size (init): size of the square
+    """
+    def __init__(self, size):
+        self._size = size
