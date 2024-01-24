811eeac97e01a97b1c49993ea7e47578234b645e
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-24 08:42:21 -0800

    3-square.py file added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square class with exception"""
+
+
+class Square:
+    """Updated class with exception handling feature
+
+    __init__:
+        instantiate the square with a private attribute
+
+    Attributes:
+        size (int): length/breadth of a square
+
+    Raises:
+        TypeError: if size not int
+        ValueError: if size < 0
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Computes the area of a square
+
+        Args:
+            None
+
+        Returns:
+            Area (int): Area of a square
+        """
+        return (self.__size * self.__size)
