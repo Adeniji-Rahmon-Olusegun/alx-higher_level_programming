8a5b7b1b5016df0f411a43fb2a5e455819d747f2
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-24 09:44:27 -0800

    5-square.py file added

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Square class with exception"""
+
+
+class Square:
+    """Updated class with exception handling feature
+
+    __init__:
+        instantiate the square with a private attribute
+
+    Attributes:
+        size (int): length/breadth of a square
+
+    Raises:
+        TypeError: if size not int
+        ValueError: if size < 0
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the value of size
+
+        Return:
+            size (int)
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Computes the area of a square
+
+        Args:
+            None
+
+        Returns:
+            Area (int): Area of a square
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints a suare using # symbol"""
+        if self.__size == 0:
+            print()
+        else:
+            for s in range(self.__size):
+                for si in range(self.__size):
+                    print("#", end="")
+                print()
