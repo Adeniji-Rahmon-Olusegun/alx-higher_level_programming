0e7d03c6cf72a38a59d481eda20e70e99176f515
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-01-30 10:59:35 -0800

    101-locked_class.py file added

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    """Prevents the user from dynamically creating
+    new instances except if the new instance attribute
+    is called first_name
+    """
+    def __setattr__(self, p_name, p_val):
+        if p_name == "first_name":
+            self.__dict__[p_name] = p_val
+        else:
+            msg = f"'LockedClass' object has no attribute '{p_name}'"
+            raise AttributeError(msg)
