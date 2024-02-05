d265d82c95936425f09e3f1eff4ebc5261782a9a
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-05 09:27:59 -0800

    0-lookup.py and README.md files added

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Attribute and method function"""
 
 def lookup(obj):
     """Returns the list of available attributes
-    and methods of an object"""
+    and methods of an object
+    """
 
     return list(dir(obj))
