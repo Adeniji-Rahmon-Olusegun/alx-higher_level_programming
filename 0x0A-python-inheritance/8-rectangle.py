7cec5cc2bd86b9c728c0f75d4db2c20e5da19453
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-05 14:03:12 -0800

    7-base_geometry.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""This module contains class Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """This class inherits BaseGeometry properties"""
+    def __init__(self, width, height):
+
