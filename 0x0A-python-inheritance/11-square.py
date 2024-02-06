eb4512828ccafb0e307491f284cc522df3fd76de
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-06 04:32:19 -0800

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
         self.__size = size
 
     def __str__(self):
-        return f"[Square] {self.__self}/{self.__self}"
+        return f"[Square] {self.__size}/{self.__size}"
 
     def __repr__(self):
         return f"[Square] {self.__size}/{self.__size}"
