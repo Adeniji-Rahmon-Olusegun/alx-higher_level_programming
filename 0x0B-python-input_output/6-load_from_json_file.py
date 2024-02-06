0033f730bc692462cd07da55c62556b17f87c8dc
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-06 14:49:21 -0800

    4-from_json_string.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module contains save_to_json_file function"""
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an Object to a text file using JSON repr.
+
+    Args:
+        my_obj: Python Object
+        filename: file to be written to
+
+    Return:
+        None
+    """
+    with open(filename, mode="w") as json_file:
+        json.dump(my_obj, json_file)
