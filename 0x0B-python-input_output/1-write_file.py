69ca7baf9d87ace9354b7e290623bce6b5bb8997
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-06 13:02:39 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module contains write_file function"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes to a file
+
+    Args:
+        filename: file path to file
+        text: data to write
+
+    Return:
+        None
+    """
+    with open(filename, mode="w", encoding='utf-8') as file_obj:
+        file_obj.write(text)
