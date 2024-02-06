27cc1490a3531b10ad98ce93a74b97c844a278c3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-06 04:23:09 -0800

    11-square.py file added

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module contains Square class"""
+
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return f"[Square] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        return f"[Square] {self.__width}/{self.__height}"
+
+    def area(self):
+        """Computes the area of a Square"""
+        return self.__size * self.__size
