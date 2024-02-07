5cf56cb6c92f1072e1555bf44bf8ed8026c0f7ad
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-07 13:12:01 -0800

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module contains Student class"""
+
+
+class Student:
+    """
+    Defines Student attributes
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student"""
+
+        stud_info =  {"first_name": self.first_name, "last_name": self.last_name,
+                "age": self.age}
+
+        cond_1 = isinstance(attrs, list)
+
+        if attrs is not None:
+            cond_2 =  all(isinstance(element, str) for element in attrs)
+        bool_state = cond_1 and cond_2
+
+        if bool_state:
+            stud_info = {k: v for k, v in stud_info.items() if k in attrs}
+        return stud_info
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of Student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)
