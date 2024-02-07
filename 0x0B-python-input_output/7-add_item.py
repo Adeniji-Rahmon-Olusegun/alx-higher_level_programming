4575ba1df6f8b18510ca62f02683f2d391bc99ae
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-07 09:27:07 -0800

    7-add_item.py file added

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module contains functions that load, add and save data"""
+
+import sys
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+user_args = sys.argv[1:]
+
+
+try:
+    load_from_json_file("add_item.json")
+except FileNotFoundError:
+    user_list = []
+
+user_list.extend(user_args)
+
+save_to_json_file(user_list, "add_item.json")
