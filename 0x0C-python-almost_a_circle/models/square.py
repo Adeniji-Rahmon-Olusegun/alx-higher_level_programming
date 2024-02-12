1922001085454d624376f3c6ea8551ae0388910f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-12 14:56:33 -0800

    to dictionary method added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -37,3 +37,13 @@ class Square(Rectangle):
         else:
             for key, val in kwargs.items():
                 setattr(self, key, val)
+
+    def to_dictionary(self):
+        """Returns dictionary representation of Square"""
+
+        return {
+                "id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y
+        }
