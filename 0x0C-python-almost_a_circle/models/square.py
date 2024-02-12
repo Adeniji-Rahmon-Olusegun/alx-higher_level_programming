c58c3c1e941edfbb9277b77c339fb27d219effa3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-12 13:52:06 -0800

    square.py file added

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module contains Square class"""
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Sqaure inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id=None)
+        self.size = size
+
+    def __str__(self):
+        return (
+                f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.__width}"
+        )
