4a140f5be1eeec80b5ab297ff6be097390feba45
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-12 12:59:30 -0800

    display method updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,10 +75,10 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
-        """Displays a graphical representation o a rectangle"""
+        """Displays a graphical representation of a rectangle"""
 
         for len in range(self.__y):
             print()
 
         for unit_len in range(self.__height):
-            print(" " * self._x + "#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
