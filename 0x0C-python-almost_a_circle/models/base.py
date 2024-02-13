bcbac0935194524c327d793d5629d4960159cd79
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-13 04:08:42 -0800

    to_json_string method added to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Json representation of list_dictionary"""
+
+        if list_dictionaries is None:
+            return "[]"
+        return list_dictionaries.dumps()
