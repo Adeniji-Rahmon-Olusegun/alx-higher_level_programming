5238ac46270d55d0a253b632e72bef3ae1315b48
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-14 23:29:06 +0100

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -8,3 +8,5 @@ IF @@rowcount = 0 THEN
 	CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 	GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 END IF;
+
+FLUSH PRIVILEGES;
