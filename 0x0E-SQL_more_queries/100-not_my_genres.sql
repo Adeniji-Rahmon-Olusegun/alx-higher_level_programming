e8d5a2a5a7fe741e02fb265e60a6913f21731b23
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-15 21:33:54 +0100

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@ SELECT tv_genres.name AS name
 FROM tv_genres
 WHERE tv_genres.id NOT IN (
 	SELECT tv_genres.id FROM tv_genres
-	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre.id
+	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-	WHERE tv_shows.title = "DEXTER")
+	WHERE tv_shows.title = "Dexter")
 ORDER BY name;
