707bed10cec929937798e603aee063b60534da33
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-15 20:25:31 +0100

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title As title
 FROM tv_shows
-INNER JOIN tv_show_genres as tsg ON tv_show.id = tsg.shows_id
+INNER JOIN tv_show_genres as tsg ON tv_shows.id = tsg.show_id
 INNER JOIN tv_genres ON tsg.genre_id = tv_genres.id
 WHERE name = 'Comedy'
 ORDER BY title ASC;
