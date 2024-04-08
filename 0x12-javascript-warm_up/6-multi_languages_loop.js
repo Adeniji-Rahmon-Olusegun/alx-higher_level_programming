b5f973f75eac71401a1e93ba5891bf7c05ca105c
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-08 18:23:48 +0100

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const strList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let counter = 0;
+
+while (counter < 3) {
+  console.log(strList[counter]);
+  counter++;
+}
