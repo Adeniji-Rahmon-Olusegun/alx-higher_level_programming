75c4923f000c7fbc5c70020a8d662ca5e0b281f3
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-13 22:06:37 +0100

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,15 +7,14 @@ Script that lists all State objects from the database hbtn_0e_6_usa
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
 
-    db_url = "mysql://argv[1]:argv[2]@localhost:3306/argv[3]"
+    db_url = "mysql+mysqldb://argv[1]:argv[2]@localhost:3306/argv[3]"
 
     # running on the local host at port 3306 connect to mysql server
-    engine = create_engine(db_url)
+    engine = create_engine(db_url, pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
 
