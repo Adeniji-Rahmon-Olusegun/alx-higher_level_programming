eb09a4291be41e966546c8f24a4faebc7d67bb33
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-13 16:16:22 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     cursor = my_db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = sys.argv[4]")
+    cursor.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4,))
 
     states = cursor.fetchall()
 
