cfc83349b96a564e783cd927c2d8d3e50ecf06e0
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-13 16:12:34 +0100

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    my_db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+    )
+
+    cursor = my_db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = sys.argv[4]")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    my_db.close()
