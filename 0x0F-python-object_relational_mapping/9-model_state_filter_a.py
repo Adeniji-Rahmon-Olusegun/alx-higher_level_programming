34722623cd59416ef26e9dbaaf5f3b7c7020e077
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-13 23:33:53 +0100

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+Script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa
 """
 
 from sys import argv
@@ -20,7 +21,7 @@ if __name__ == "__main__":
 
     session = Session()
 
-    states = session.query(State).filter(State.name.like("%a%")).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like("%a%")).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
