ba549f486255cc05aa9bbb0c259611b48c11e77a
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-14 02:39:21 +0100

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     session = Session()
 
-    cities = session.query(City, State).join(State).order_by(City.id).all()
+    cities = session.query(City, State).join(State).order_by(City.id)
 
     for city, state in cities:
         print(f"{state.name}: ({city.id}) {city.name}")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,11 +5,8 @@ Python file that contains the class definition of a City
 and an instance Base = declarative_base()
 """
 
-from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
-
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
@@ -20,5 +17,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State", back_populates="cities")
