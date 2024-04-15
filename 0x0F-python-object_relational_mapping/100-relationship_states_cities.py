65459f9ba40dd48ce6a9be11f7233fb54c683f60
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-15 16:25:17 +0100

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -18,6 +18,8 @@ if __name__ == "__main__":
     # running on the local host at port 3306 connect to mysql server
     engine = create_engine(db_url, pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
 
     session = Session()
