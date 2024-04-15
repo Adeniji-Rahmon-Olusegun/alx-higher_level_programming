c0037bd72525e51abd9b866fe6b699e6157f6eaa
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-15 17:31:41 +0100

    101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
     # running on the local host at port 3306 connect to mysql server
     engine = create_engine(db_url, pool_pre_ping=True)
 
-    # Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    states = session.query(State).order_by(State.id, City.id).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
