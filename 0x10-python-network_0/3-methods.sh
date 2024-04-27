d7cf88a68fe6e5c961e4adc027eb77f3f37b6497
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-27 13:59:16 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  script that takes in a URL and displays all HTTP methods the server will accept
-curl -siX OPTIONS $1 | grep -i allow
+curl -siX OPTIONS $1 | grep -i allow | cut -d ":" -f2 | tr -d '[:space]'
