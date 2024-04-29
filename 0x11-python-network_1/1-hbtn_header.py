7c40da47088185213751893dbfd9949fb34d82d5
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-29 17:54:47 +0100

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,4 @@ import sys
 import urllib.request as url
 
 with url.urlopen(sys.argv[1]) as res:
-    headers = res.headers
-    if "X-Request-Id" in headers:
-        print(headers["X-Request-Id"])
+    print(res.getheader("X-Request-Id"))
