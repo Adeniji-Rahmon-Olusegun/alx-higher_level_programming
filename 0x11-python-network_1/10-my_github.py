28b2f0afb92ea54cdf1ea82699749c62d797ac8f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-29 23:24:58 +0100

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 """
-script that takes your GitHub credentials 
+script that takes your GitHub credentials
 (username and password) and uses the GitHub API to display your id
 """
-
 
 if __name__ == "__main__":
     import sys
