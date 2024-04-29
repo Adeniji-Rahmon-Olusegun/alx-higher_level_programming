236deabf3e4b0925dcf2c89668751c8a0a465b3a
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-29 22:57:46 +0100

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+script that takes your GitHub credentials 
+(username and password) and uses the GitHub API to display your id
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = "https://api.github.com/user"
+
+    res = requests.get(url, auth=(username, password))
+
+    if res.status_code == 200:
+        json_data = res.json()
+        print(f"{json_data['id']}")
