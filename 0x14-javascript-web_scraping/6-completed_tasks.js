0c2a9a97303249ad5d88c5a413a8e8e45af86923
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-05-22 02:45:46 +0100

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+const apiEndpoint = process.argv[2];
+
+req(apiEndpoint, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const tasks = JSON.parse(body);
+    const finishedTasks = {};
+
+    tasks.forEach(todo => {
+      if (todo.completed) {
+	      if (!finishedTasks[todo.userId]) {
+		      finishedTasks[todo.userId] = 1;
+	      } else {
+		      finishedTasks[todo.userId]++;
+	      }
+      }
+    });
+
+    console.log(finishedTasks);
+  }
+});
